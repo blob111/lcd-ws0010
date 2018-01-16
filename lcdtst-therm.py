511811c8f1eb30f8c54638d8c0b5b23f05967dcc
blob111 <yooozh@gmail.com>
2018-01-17 00:35:36 +0300

    lcdtst-therm.py: - Work in progress.

--- lcdtst-therm.py
+++ lcdtst-therm.py
@@ -3,11 +3,14 @@
 # 1st line: date[DD.MM.YYYY] time[HH:MM]
 # 2nd line: id[IIII] err[EE%] temp[+NN.N]
 
+import os
 import sys
 import select
 import signal
 import fcntl
-from math import gcd
+import time
+import struct
+from fractions import gcd
 import ws0010
 from w1thermsensor import W1ThermSensor
 
@@ -26,6 +29,14 @@ SENSOR_READ_INTERVAL = 60
 SENSOR_DISP_INTERVAL = 10
 CLOCK_INTERVAL = 5
 
+SIG_WAKEUP_FD_RLEN = 8  # length of data read from signal wakeup file descriptor
+
+cleanup_objects = {
+    'poller': None,
+    'pipe_r': None,
+    'pipe_w': None,
+}
+
 def disp_init():
     """Initialize display."""
 
@@ -33,6 +44,16 @@ def disp_init():
     lcd.emode_set(increment=True)
     lcd.dispctl_set(disp_on=True, curs_on=True, blink_on=True)
     return lcd
+
+def disp_clock(lcd):
+    """Display clock."""
+
+    return
+
+def disp_sensor(lcd, sensor):
+    """Display sensor value."""
+
+    return
 
 def read_sensor(sensor):
     """Save sensor's value."""
@@ -44,36 +65,143 @@ def read_sensor(sensor):
         sensor['obj'].value = W1_THERM_SCALE_FACTOR(val)
         sensor['read_success'] += 1
 
+def signal_handler(signal, frame):
+    """Signal handler."""
+
+    return
+
+def cleanup():
+    """Cleanup routine."""
+
+    sys.stderr.write('INFO: Clean-up\n')
+    signal.setitimer(signal.ITIMER_REAL, 0)
+    if cleanup_objects['poller']:
+        cleanup_objects['poller'].close()
+    if cleanup_objects['pipe_r']:
+        os.close(cleanup_objects['pipe_r'])
+    if cleanup_objects['pipe_w']:
+        os.close(cleanup_objects['pipe_w'])
+
 def main():
     """Main program."""
 
     sensors = []
-    itimer_factor = {}
-    itimer_current = {}
+    itimer_next = {}
 
     # Initialize sensors
     for sensor in W1ThermSensor.get_available_sensors():
-        sensors.add({obj=sensor, read_success=0, read_crc=0, read_nan=0, value=None})
+        sensors.append({'obj': sensor, 'read_success': 0, 'read_crc': 0, 'read_nan': 0, 'value': None})
     if len(sensors) == 0:
-        sys.stderr.write('\nNo sensors found\n')
+        sys.stderr.write('\nERROR: No sensors found\n')
         exit(0)
+    else:
+        sys.stderr.write('\nINFO: Found {} sensors\n'.format(len(sensors)))
+        active_sensor_idx = 0
 
-    # Calculate interval timer value and bounded variables
-    itimer_value = gcd(gcd(SENSOR_READ_INTERVAL, SENSOR_DISP_INTERVAL), CLOCK_INTERVAL)
-    itimer_factor['sensor_read_interval'] = int(SENSOR_READ_INTERVAL / itimer_value)
-    itimer_current['sensor_read_interval'] = itimer_factor['sensor_read_interval']
-    itimer_factor['sensor_disp_interval'] = int(SENSOR_DISP_INTERVAL / itimer_value)
-    itimer_current['sensor_disp_interval'] = itimer_factor['sensor_disp_interval']
-    itimer_factor['clock_interval'] = int(CLOCK_INTERVAL / itimer_value)
-    itimer_current['clock_interval'] = itimer_factor['clock_interval']
+    # Initialize LCD
+    lcd = disp_init()
 
     # Initialize signal file descriptor
     # We must set write end of pipe to non blocking mode
     # Also we don't want to block while read signal numbers from read end
     pipe_r, pipe_w = os.pipe()
+    cleanup_objects['pipe_r'] = pipe_r
+    cleanup_objects['pipe_w'] = pipe_w
     flags = fcntl.fcntl(pipe_w, fcntl.F_GETFL, 0)
     fcntl.fcntl(pipe_w, fcntl.F_SETFL, flags | os.O_NONBLOCK)
     signal.set_wakeup_fd(pipe_w)
     flags = fcntl.fcntl(pipe_r, fcntl.F_GETFL, 0)
     fcntl.fcntl(pipe_r, fcntl.F_SETFL, flags | os.O_NONBLOCK)
 
+    # Redefine signal handlers
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGHUP, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    # Calculate interval timer value and bounded variables
+    itimer_value = gcd(gcd(SENSOR_READ_INTERVAL, SENSOR_DISP_INTERVAL), CLOCK_INTERVAL)
+    t = time.time()
+    itimer_next['sensor_read_interval'] = t + SENSOR_READ_INTERVAL
+    itimer_next['sensor_disp_interval'] = t + SENSOR_DISP_INTERVAL
+    itimer_next['clock_interval'] = t + CLOCK_INTERVAL
+
+    # Create poller and register file descriptors
+    poller = select.epoll()
+    cleanup_objects['poller'] = poller
+    poller.register(pipe_r, select.EPOLLIN)
+
+    # Set interval timer
+    # Initial value of timer bounded to measurement itimer_value
+    t = time.time()
+    t_rest = itimer_value - t % itimer_value
+    if t_rest < 0:
+        t_rest += itimer_value
+    signal.setitimer(signal.ITIMER_REAL, t_rest, itimer_value)
+
+    # Main loop
+    sys.stderr.write('INFO: Entering main loop\n')
+    while True:
+    
+        # Wait for events and process its
+        try:
+            events = poller.poll()
+        except InterruptedError:
+            continue
+        for fd, flags in events:
+    
+            # Signal received, extract signal numbers from wakeup fd
+            if fd == pipe_r and flags & select.EPOLLIN:
+                data = os.read(pipe_r, SIG_WAKEUP_FD_RLEN)
+                signums = struct.unpack('{}B'.format(len(data)), data)
+
+                # Process signals
+                for signum in signums:
+                    if signum == signal.SIGALRM:
+
+                        # Display clock
+                        if t >= itimer_next['clock_interval'] <= time.time():
+                            disp_clock(lcd)
+                            while itimer_next['clock_interval'] <= time.time():
+                                itimer_next['clock_interval'] += CLOCK_INTERVAL
+
+                        # Display sensor
+                        if t >= itimer_next['sensor_disp_interval']:
+                            disp_sensor(lcd, sensors[active_sensor_idx])
+                            active_sensor_idx += 1
+                            if active_sensor_idx >= len(sensors):
+                                active_sensor_idx = 0
+                            while itimer_next['sensor_disp_interval'] <= time.time():
+                                itimer_next['sensor_disp_interval'] += SENSOR_DISP_INTERVAL
+
+                        # Read sensors
+                        if t >= itimer_next['sensor_read_interval']:
+                            for sensor in sensors:
+                                read_sensor(sensor)
+                            while itimer_next['sensor_read_interval'] <= time.time():
+                                itimer_next['sensor_read_interval'] += SENSOR_READ_INTERVAL
+
+                    elif signum == signal.SIGINT:
+                        sys.stderr.write('\nINFO: SIGINT received\n')
+                        cleanup()
+                        sys.exit(0)
+                    elif signum == signal.SIGTERM:
+                        sys.stderr.write('\nINFO: SIGTERM received\n')
+                        cleanup()
+                        sys.exit(0)
+                    elif signum == signal.SIGHUP:
+                        sys.stderr.write('INFO: SIGHUP received\n')
+                    else:
+                        sys.stderr.write('WARNING: Unexpected signal received: {}\n'.format(signum))
+
+            # Unexpected event
+            else:
+                sys.stderr.write('ERROR: Unexpected event on fd {}, flags {}\n'.format(fd, flags))
+
+# Call main routine
+main()
+
+# This point should be never reached
+# Cleanup and exit
+cleanup()
+sys.exit(0)
